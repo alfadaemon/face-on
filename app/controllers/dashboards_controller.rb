fd713aba29c220510467292c27c4268bf76f9063
DiegoTc <diegoturciostc@gmail.com>
2014-03-30 12:15:08 -0600

    Agregando

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,9 @@ class DashboardsController < ApplicationController
   # GET /dashboards
   # GET /dashboards.json
   def index
+    @dashboard = 0
     @criminals = Criminal.take(12)
+    #@criminals = Criminal.where("genero = '0'")
   end
 
   # GET /dashboards/1
@@ -15,11 +17,30 @@ class DashboardsController < ApplicationController
     @criminals = Criminal.all(:limit=>10)
   end
 
+  def filtro
+    
+  end
+
+  # POST /dashboards
+  # POST /dashboards.json
+  def create
+    @dashboard = Dashboard.new(dashboard_params)
+
+    respond_to do |format|
+      if @dashboard.save
+        format.html { redirect_to @dashboard, notice: 'Dashboard was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @dashboard }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @dashboard.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_dashboard
-      @criminal = Criminal.find(params[:id])
+    #  @criminal = Criminal.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
